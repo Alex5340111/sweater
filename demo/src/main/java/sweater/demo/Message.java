dd67fd9ff7de37e63f1fdd05cdc1f102aec2b5a7
user1 <alex5340111@gmail.com>
2020-07-05 21:10:32 +0300

    change mustache on the freemarker

--- demo/src/main/java/sweater/demo/Message.java
+++ demo/src/main/java/sweater/demo/Message.java
@@ -9,6 +9,7 @@ public class Message {
     private Integer id;
     private String text;
     private String tag;
+
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
     private User author;
--- demo/src/main/java/sweater/demo/controller/RegistrationController.java
+++ demo/src/main/java/sweater/demo/controller/RegistrationController.java
@@ -32,6 +32,6 @@ public class RegistrationController {
         user.setRoles(Collections.singleton(Role.USER));
         userRepo.save(user);
         //noinspection SpringMVCViewInspection
-        return "redirect:/login";
+        return "login";
     }
 }
